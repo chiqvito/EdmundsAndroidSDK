8ec195671463dce83fb288ae9347da063fb7b5b6
chiqvito <grzegorz@bialek.it>
2015-04-29 16:04:36 +0200

    implementation parcelable

--- edmunds-sdk/src/main/java/pl/chiqvito/edmunds/sdk/dto/vehicle/response/MakesDTO.java
+++ edmunds-sdk/src/main/java/pl/chiqvito/edmunds/sdk/dto/vehicle/response/MakesDTO.java
@@ -1,16 +1,54 @@
 package pl.chiqvito.edmunds.sdk.dto.vehicle.response;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MakesDTO {
+public class MakesDTO implements Parcelable {
+
+    public static final Parcelable.Creator<MakesDTO> CREATOR = new Parcelable.Creator<MakesDTO>() {
+        public MakesDTO createFromParcel(Parcel in) {
+            return new MakesDTO(in);
+        }
+
+        public MakesDTO[] newArray(int size) {
+            return new MakesDTO[size];
+        }
+    };
 
     @SerializedName("makes")
     private List<MakeDTO> makes;
 
     @SerializedName("makesCount")
     private Integer makesCount;
+
+    public MakesDTO() {
+    }
+
+    private MakesDTO(Parcel in) {
+        readFromParcel(in);
+    }
+
+    private void readFromParcel(Parcel in) {
+        makesCount = (Integer) in.readValue(Integer.class.getClassLoader());
+        makes = new ArrayList<MakeDTO>();
+        in.readList(makes, MakeDTO.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(makesCount);
+        dest.writeList(makes);
+    }
 
     public List<MakeDTO> getMakes() {
         return makes;
--- edmunds-sdk/src/main/java/pl/chiqvito/edmunds/sdk/dto/vehicle/response/ModelsDTO.java
+++ edmunds-sdk/src/main/java/pl/chiqvito/edmunds/sdk/dto/vehicle/response/ModelsDTO.java
@@ -1,16 +1,54 @@
 package pl.chiqvito.edmunds.sdk.dto.vehicle.response;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ModelsDTO {
+public class ModelsDTO implements Parcelable {
+
+    public static final Parcelable.Creator<ModelsDTO> CREATOR = new Parcelable.Creator<ModelsDTO>() {
+        public ModelsDTO createFromParcel(Parcel in) {
+            return new ModelsDTO(in);
+        }
+
+        public ModelsDTO[] newArray(int size) {
+            return new ModelsDTO[size];
+        }
+    };
 
     @SerializedName("models")
     private List<ModelDTO> models;
 
     @SerializedName("modelsCount")
     private Integer modelsCount;
+
+    public ModelsDTO() {
+    }
+
+    private ModelsDTO(Parcel in) {
+        readFromParcel(in);
+    }
+
+    private void readFromParcel(Parcel in) {
+        modelsCount = (Integer) in.readValue(Integer.class.getClassLoader());
+        models = new ArrayList<ModelDTO>();
+        in.readList(models, ModelDTO.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(modelsCount);
+        dest.writeList(models);
+    }
 
     public List<ModelDTO> getModels() {
         return models;
